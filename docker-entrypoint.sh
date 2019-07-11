32bfac8f82461ff1c24402a8a62f994c59a8ad39
Ivan Cvitkovic <icvitkovic@gmail.com>
2019-07-11 01:14:16 -0700

    Add util function for changing settings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,8 +8,21 @@ if [ -d "$DIR" ]; then
     run-parts --verbose "$DIR"
 fi
 
+
+update_setting() {
+    # update a setting in /etc/cobbler/settings
+    local setting_name="$1"
+    local setting_value="$2"
+
+    local default_settings_file=/etc/cobbler/settings
+    local settings_file="${3:-$default_settings_file}"
+
+    sed  -i -e '/^'$setting_name'/ s/:.*$/: '$setting_value'/' $settings_file
+}
+
+
 # update cobblerd host
-sed  -i -e '/^server/ s/:.*$/: cobblerd/' /etc/cobbler/settings
+update_setting server cobblerd
 
 for iso_file in /media/*.iso; do
     test -e "$iso_file" || continue
