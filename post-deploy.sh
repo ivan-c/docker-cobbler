1fc7709c784cd82311d852d941a1a0fb6b5d74e9
Ivan Cvitkovic <icvitkovic@gmail.com>
2019-07-12 01:16:17 -0700

    Import all install images

--- post-deploy.sh
+++ post-deploy.sh
@@ -8,3 +8,14 @@ cobbler='docker-compose exec cobblerd cobbler'
 
 echo rebuilding configs...
 $cobbler sync
+
+echo importing extracted images...
+for extracted_image_dir in media/*; do
+    test -d "$extracted_image_dir" || continue
+    image_name="$(basename $extracted_image_dir)"
+
+    echo importing $extracted_image_dir
+    $cobbler import \
+        --path="$extracted_image_dir" \
+        --name="$image_name"
+done
