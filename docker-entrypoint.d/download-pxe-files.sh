83bb0de4d90a9988c239281c28d6fa2d316c1b45
Ivan Cvitkovic <icvitkovic@gmail.com>
2019-08-14 22:08:06 -0700

    Add script for downloading PXE files

---
+++ docker-entrypoint.d/download-pxe-files.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -eu
+
+# extract each iso to a separate directory
+mirror_base=https://mirrors.edge.kernel.org
+mirror_path=/debian/dists/stable/main/installer-amd64/current/images/netboot/debian-installer/amd64/
+
+pxe_dir=/media/debian-stretch-pxe
+
+mkdir --parents $pxe_dir
+
+for pxe_filename in initrd.gz linux; do
+    test ! -f "${pxe_dir}/${pxe_filename}" || continue
+
+    curl \
+        "${mirror_base}${mirror_path}/${pxe_filename}" \
+        -o "${pxe_dir}/${pxe_filename}"
+done
