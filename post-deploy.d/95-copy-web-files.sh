baf4b12dce9c89a1bca627addc5de0cab31bd7f7
Ivan Cvitkovic <icvitkovic@gmail.com>
2019-09-24 22:03:13 -0700

    Add comment

--- post-deploy.d/95-copy-web-files.sh
+++ post-deploy.d/95-copy-web-files.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -eu
 
-
+# overwrite broken relative symlinks with real files
+# todo: fixup symlinking/volumes
 cp --remove-destination --verbose --recursive \
     /var/lib/tftpboot/images \
     /var/www/cobbler/
